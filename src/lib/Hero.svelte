2a63b391bf7a9e13ebb3cc16af4e28dd1a86a188
Om Thorat <76207818+Om-Thorat@users.noreply.github.com>
2023-12-18 11:53:08 +0530

    basic front

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,9 @@
+
+<div class="flex flex-col justify-end gap-4 h-[40%] w-[50%]">
+    <span class="text-8xl font-mono font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#12c2e9] via-[#c471ed] to-[#f64f59]">Aperta Fons</span>
+    <span class="text-xl w-full ">A month long Opensource challenge</span>
+    <div class="flex gap-12 w-[100%]">
+        <button class="btn">Dashboard</button>
+        <button class="btn">Discord</button>
+    </div>
+</div>
---
+++ src/lib/Timer.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+    let days = "99";
+    let hours = "99";
+    let mins = "99";
+    let secs = "99";
+    import { sleep } from "../scripts/utils";
+    export let final:string;
+    async function countdown(){
+        const end = new Date(final)
+        while (true){
+            const date = new Date()
+            var diffdate:Date = new Date(end - date)
+            days = diffdate.getDate().toString().padStart(2,'0')
+            hours = diffdate.getHours().toString().padStart(2,'0')
+            mins = diffdate.getMinutes().toString().padStart(2,'0')
+            secs = diffdate.getSeconds().toString().padStart(2,'0')
+            await sleep(1);
+        }
+    }
+    onMount(countdown)
+</script>
+
+<div class="flex flex-col gap-3 items-start w-[50%]">
+    <span class="text-lg">The contest is live until</span>
+    <span class="text-5xl font-mono font-extrabold">
+        {days}:{hours}:{mins}:{secs}
+    </span>
+</div>
